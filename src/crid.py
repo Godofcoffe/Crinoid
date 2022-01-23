8257c7b3eb7416a4db0d734e62da32ac2f48a1d8
Kevin G. Tertuliano <robotnyte@gmail.com>
2022-01-23 11:49:29 -0300

    fix: small changes

--- src/crid.py
+++ src/crid.py
@@ -5,19 +5,11 @@ from pynput import keyboard
 buffer = []
 __version__ = "0.3.0"
 
-
 # TODO:thread para iterar a frase na lista e colorir a palavra com a syntax python
 
 
-# def on_release(key):
-#     print('{0} released'.format(
-#         key))
-#     if key == ".":
-#         return False
-#
-#
-# with keyboard.Listener(on_release=on_release) as listener:
-#     listener.join()
+file_name = "foo.txt"
+
 
 def save():
     if input("Save buffer? [y/n]: ").lower().strip() == "y":
@@ -26,16 +18,17 @@ def save():
             file.write(L + "\n")
 
 
-try:
-    if 1 < len(sys.argv) < 3:
-        if len(sys.argv) == 1:
-            file_name = "foo.txt"
-        else:
-            file_name = sys.argv[1]
-        if isfile(file_name) and exists(file_name):
-            file = open(file_name)
-        else:
-            file = open(file_name, "w+")
+if 1 < len(sys.argv) < 3:
+    if not len(sys.argv) == 1:
+        file_name = sys.argv[1]
+    else:
+        print("crid.py <FILE>")
+
+if isfile(file_name) and exists(file_name):
+    print(open(file_name).read(), end="")
+else:
+    file = open(file_name, "w+")
+    try:
         line = 0
         empty = 0
         while True:
@@ -49,9 +42,7 @@ try:
             else:
                 empty = 0
             line += 1
-    else:
-        print("crid.py <FILE>")
-except KeyboardInterrupt:
-    save()
-finally:
-    file.close()
+    except KeyboardInterrupt:
+        save()
+    finally:
+        file.close()
